3b3af9e6874d9ac6e1b6f2748d259ff4b00da576
newprakorn <100191.new.prakorn@gmail.com>
2026-06-21 09:09:45 +0700

    chore: setup development tooling

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,17 +1,5 @@
 function App() {
-  return (
-    <main className="min-h-screen bg-slate-100 flex items-center justify-center">
-      <div className="rounded-xl bg-white p-10 shadow-xl text-center">
-        <h1 className="text-4xl font-bold text-blue-600">
-          Smart Factory Platform
-        </h1>
-
-        <p className="mt-4 text-slate-600">
-          Phase 2 : Tailwind CSS พร้อมใช้งาน 🎉
-        </p>
-      </div>
-    </main>
-  )
+  return <div className="flex items-center justify-center bg-blue-500 p-2 text-white">Hello</div>
 }
 
 export default App
